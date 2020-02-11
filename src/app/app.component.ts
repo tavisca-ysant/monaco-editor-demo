6e7a7f83027c5dd5e184fba209305cc5535fbc08
tavisca-ysant <ysant@tavisca.com>
2020-02-11 16:11:44 +0530

    Added component for nav-bar and a save button to save the code into a temporary variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'monaco-editor-demo';
-  editorOptions = {theme: 'vs-dark', language: 'javascript'};
-  code: string= 'function x() {\nconsole.log("Hello world!");\n}';
+  editorOptions = {theme: 'vs-dark', language: 'csharp'};
+  // code: string= 'function x() {\nconsole.log("Hello world!");\n}';
+  code: string;
   onInit(editor) {
       let line = editor.getPosition();
       console.log(line);
     }
+
+    onChange(editorCode){
+      //console.log(editorCode);
+      this.code = editorCode;
+    }
+
+    saveCodeChanges(input){
+     // this.code = input;
+      console.log(input);
+      console.log(this.code);
+      alert('Your code has been successfully saved!!');
+     // location.reload();
+    }
+
 }
